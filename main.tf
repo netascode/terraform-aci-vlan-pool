13b4edfefc2853709e0ff1ed6aeaf0b8d7c0ee1f
danischm <danischm@cisco.com>
2021-10-11 20:34:46 +0200

    Update implicit resource dependencies

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aci_rest" "fvnsVlanInstP" {
 
 resource "aci_rest" "fvnsEncapBlk" {
   for_each   = { for range in var.ranges : range.from => range }
-  dn         = "${aci_rest.fvnsVlanInstP.id}/from-[vlan-${each.value.from}]-to-[${each.value.to == null ? "vlan-${each.value.from}" : "vlan-${each.value.to}"}]"
+  dn         = "${aci_rest.fvnsVlanInstP.dn}/from-[vlan-${each.value.from}]-to-[${each.value.to == null ? "vlan-${each.value.from}" : "vlan-${each.value.to}"}]"
   class_name = "fvnsEncapBlk"
   content = {
     from      = "vlan-${each.value.from}"
